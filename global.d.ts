842f4e5d36bc0a60e2de0c7125a39a4a5a77164e
BhawickJain <bhawick@outlook.com>
2022-11-10 12:16:13 +0000

    allow module.css
    not sure why this was missing

---
+++ global.d.ts
@@ -0,0 +1,3 @@
+declare module "*.css";
+// https://stackoverflow.com/questions/69056475/cannot-find-module-module-css-or-its-corresponding-type-declarations
+// TODO weird I need this, check if can be removed
